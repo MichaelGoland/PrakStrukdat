bdeaddd32656a090ebe2170dc23d1d22dab8651c
MichaelGoland <93329954+MichaelGoland@users.noreply.github.com>
2022-08-29 18:15:28 +0700

    Add files via upload

---
+++ Latihan1_Golland.py
@@ -0,0 +1,22 @@
+
+angka1 = int(input("Masukkan angka pertama : "))
+angka2 = int(input("Masukkan angka kedua : "))
+
+#tambah
+print("{} + {} = ".format(angka1,angka2))
+print(angka1 + angka2)
+
+#pengurangan
+print("{} - {} = ".format(angka1,angka2))
+print(angka1-angka2)
+
+#perkalian
+print("{} X {} = ".format(angka1,angka2))
+print(angka1 * angka2)
+
+#pembagian
+print("{} : {} = ".format(angka1,angka2))
+print(int(angka1 / angka2))
+
+
+
